4e463936fffe52cde037b540bca02a5d92db493e
FringeY <465964690@qq.com>
2020-06-21 00:28:53 +0800

    feat: support build by rollup

--- packages/preset-wind-component/src/type/rollup-plugin.d.ts
+++ packages/preset-wind-component/src/type/rollup-plugin.d.ts
@@ -1,6 +1,3 @@
-declare module 'rollup-plugin-babel';
-declare module 'rollup-plugin-json';
-declare module 'rollup-plugin-node-resolve';
-declare module 'rollup-plugin-commonjs';
-declare module 'rollup-plugin-less';
-declare module 'rollup-plugin-typescript';
+declare module '@rollup/plugin-babel';
+declare module '@rollup/plugin-json';
+declare module 'rollup-plugin-auto-external';
