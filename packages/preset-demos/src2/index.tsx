f0d620b9cb9250149cd366347153cd7efcf4457a
csr632 <632882184@qq.com>
2020-10-13 14:53:47 +0800

    fix: clean up

--- packages/preset-demos/src2/index.tsx
+++ packages/preset-demos/src2/index.tsx
@@ -5,9 +5,6 @@ import {
   mount,
 } from "@alicloud/console-os-react-portal";
 
-import "./index.less";
-// import "@alife/alicloud-components/dist/xconsole.css?fusion-prefix"
-
 import DemoLoader, { demoKeys } from "./DemoLoader";
 
 registerExposedModule("demoKeys", demoKeys);
