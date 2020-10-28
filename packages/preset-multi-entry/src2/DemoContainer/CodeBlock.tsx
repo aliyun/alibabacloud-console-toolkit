164377639396165301acf4df99c4fd9686493308
csr632 <632882184@qq.com>
2020-10-28 16:44:40 +0800

    fix CodeBlock style

--- packages/preset-multi-entry/src2/DemoContainer/CodeBlock.tsx
+++ packages/preset-multi-entry/src2/DemoContainer/CodeBlock.tsx
@@ -1,2 +1,13 @@
+import React from "react";
 import { markdownComponents } from "@alicloud/console-toolkit-markdown-renderer";
-export default markdownComponents.code;
+
+export default props => {
+  return (
+    <markdownComponents.code
+      {...props}
+      style={{
+        margin: 0
+      }}
+    />
+  );
+};
