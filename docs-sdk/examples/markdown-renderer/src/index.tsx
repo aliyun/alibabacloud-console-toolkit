8393087e23b5dd6150e9d6dcaaa5c116074a6276
csr632 <632882184@qq.com>
2020-12-14 15:44:54 +0800

    update example

--- docs-sdk/examples/markdown-renderer/src/index.tsx
+++ docs-sdk/examples/markdown-renderer/src/index.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import { render } from "react-dom";
-import * as ReactDOM from "react-dom";
-
 import { MarkdownRenderer } from "@alicloud/console-toolkit-markdown-renderer";
 
 // @ts-ignore
@@ -9,46 +6,75 @@ import md from "!!raw-loader!./testMd.md";
 
 import "./index.less";
 
+import React from "react";
+import * as ReactDOM from "react-dom";
+import * as styled from "styled-components";
+import * as components from "@alicloud/console-components";
+import * as moment from "moment";
+
+import "@alicloud/console-components/dist/xconsole.css";
+
 const commonDeps = {
   react: React,
-  "react-dom": ReactDOM
+  "react-dom": ReactDOM,
+  "styled-components": styled,
+  "@alicloud/console-components": components,
+  moment: moment,
+  "@alicloudfe/components": components,
 };
 
 const consoleOSAppInfo = {
   "alicloud-component-demos": {
     servePath:
-      "https://opensource-microapp.oss-cn-hangzhou.aliyuncs.com/app/alicloud-component-docs/",
+      "https://opensource-microapp.oss-cn-hangzhou.aliyuncs.com/app/breezr-docs/%40alicloudfe/components/-alpha/",
     deps: {
-      ...commonDeps
-    }
+      ...commonDeps,
+    },
+    demoOpts: {
+      modifyDisplayCode: ({ code, meta }) => {
+        return code
+          .replace(/xdemo-/g, "next-")
+          .replace(/@alicloudfe\/components/g, "@alicloud/console-components");
+      },
+    },
   },
   "xconsole-demos": {
-    // servePath: 'http://localhost:3335/',
-    servePath: "https://dev.g.alicdn.com/xconsole/demos/0.1.1/",
+    servePath: "https://g.alicdn.com/xconsole/demos/0.1.1/",
     deps: {
-      ...commonDeps
-    }
-  },
-  "xconsole-chart-demos": {
-    servePath: "https://dev.g.alicdn.com/txddp/console-charts-demos/0.2.0/",
-    deps: {
-      ...commonDeps
-    }
+      ...commonDeps,
+    },
+    demoOpts: {
+      modifyDisplayCode: ({ code, meta }) => {
+        return code.replace(/xdemo-/g, "next-");
+      },
+    },
   },
   "breezr-blocks": {
-    servePath: "https://dev.g.alicdn.com/breezr/breezr-block/0.1.0/",
+    servePath: "https://g.alicdn.com/breezr/breezr-block/0.1.4/",
     deps: {
-      ...commonDeps
-    }
-  }
+      ...commonDeps,
+    },
+  },
+  "xconsole-chart-demos": {
+    servePath: "https://g.alicdn.com/txddp/console-charts-demos/0.3.0/",
+    deps: {
+      ...commonDeps,
+    },
+  },
 };
 
 render(
   <MarkdownRenderer
     source={md}
     toc
-    resolveAppServePath={appId => {
+    resolveAppServePath={(appId) => {
       return consoleOSAppInfo[appId]?.servePath;
+    }}
+    resolveDemoOpts={(appId) => {
+      return consoleOSAppInfo[appId]?.demoOpts;
+    }}
+    resolveAppDeps={(appId) => {
+      return consoleOSAppInfo[appId]?.deps;
     }}
   />,
   document.querySelector(".app")
