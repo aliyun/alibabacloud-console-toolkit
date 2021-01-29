5dfa2084cb71936e708759e8911d0771bc7f9fb8
csr632 <632882184@qq.com>
2021-01-29 12:10:09 +0800

    feat(breezr-docs): find demos by default

--- docs-sdk/docs-provider/src/index.ts
+++ docs-sdk/docs-provider/src/index.ts
@@ -1,5 +1,7 @@
 import presetWind from "@alicloud/console-toolkit-preset-official";
+import globby from "globby";
 import * as path from "path";
+import * as fs from "fs-extra";
 import * as Chain from "webpack-chain";
 export type { IDemoOpts } from "@alicloud/console-toolkit-docs-shared";
 import { getEnv } from "@alicloud/console-toolkit-shared-utils";
@@ -45,6 +47,25 @@ export default (params: IParams, args) => {
     params.output = env.buildDestDir;
   } else {
     params.output = params.output ?? "doc-dist";
+  }
+  if (!params.getDemos) {
+    const cwd = env.workingDir ?? process.cwd();
+    const baseDir = path.resolve(cwd, "demos");
+    // 默认从demos目录查找demo
+    if (fs.existsSync(baseDir)) {
+      params.getDemos = () => {
+        const paths = globby.sync("**/*.demo.tsx", { cwd: baseDir });
+        return paths.map((relativePath) => {
+          //  const fileName = path.basename(relativePath)
+          const demoName = relativePath.replace(/\.demo\.tsx?$/, "");
+          // 对于每个demo，要返回demo key和demo路径
+          return {
+            key: demoName,
+            path: path.resolve(baseDir, relativePath),
+          };
+        });
+      };
+    }
   }
   params.consoleOSId = params.consoleOSId || "console-os-demos";
 
