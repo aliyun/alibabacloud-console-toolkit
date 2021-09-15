abf00909d45ccfbba437e5212783c3735d3a68b0
csr632 <632882184@qq.com>
2021-09-15 14:58:30 +0800

    prettier code style

--- packages/shared-utils/src/fork.ts
+++ packages/shared-utils/src/fork.ts
@@ -1,13 +1,13 @@
-import { fork as forkChild } from "child_process";
-import { debug } from "./debug";
+import { fork as forkChild } from 'child_process';
+import { debug } from './debug';
 
-export const DONE = "DONE";
-export const STARTING = "STARTING";
-export const RESTART = "RESTART";
+export const DONE = 'DONE';
+export const STARTING = 'STARTING';
+export const RESTART = 'RESTART';
 
 function send(message: any) {
   if (process.send) {
-    debug("shared-utils", `send ${JSON.stringify(message)}`);
+    debug('shared-utils', `send ${JSON.stringify(message)}`);
     process.send(message);
   }
 }
@@ -17,14 +17,14 @@ export function fork(scriptPath: string) {
 
   // 替换--inspect参数，防止错误
   // “Starting inspector on 127.0.0.1:9229 failed: address already in use”
-  const currentInspectArgIdx = execArgv.findIndex(a =>
-    a.startsWith("--inspect")
+  const currentInspectArgIdx = execArgv.findIndex((a) =>
+    a.startsWith('--inspect')
   );
   if (currentInspectArgIdx >= 0) {
     const currentInspectArg = execArgv[currentInspectArgIdx];
-    const currentPort = Number(currentInspectArg.split("=")[1] || "9229");
+    const currentPort = Number(currentInspectArg.split('=')[1] || '9229');
     const forkInspectArg =
-      currentInspectArg.split("=")[0] + "=" + String(currentPort + 1);
+      currentInspectArg.split('=')[0] + '=' + String(currentPort + 1);
     console.log(
       `发现当前进程有node inspect参数"${currentInspectArg}". 因此我们fork的进程时候也加入node inspect参数"${forkInspectArg}"`
     );
@@ -33,7 +33,7 @@ export function fork(scriptPath: string) {
 
   const child = forkChild(scriptPath, process.argv.slice(2), { execArgv });
 
-  child.on("message", data => {
+  child.on('message', (data) => {
     const type = (data && data.type) || null;
     if (type === RESTART) {
       child.kill();
