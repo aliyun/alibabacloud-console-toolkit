179608abf6e3276aa75f28c095b672d870ebf97b
liusi.xbw <liusi.xbw@alibaba-inc.com>
2021-12-16 15:54:02 +0800

    fix: error for public path

--- packages/plugin-ssr/src/index.ts
+++ packages/plugin-ssr/src/index.ts
@@ -56,7 +56,7 @@ export default async (api: PluginAPI, options: IOption) => {
       }
     })
     if (microApp) {
-      api.dispatchSync('configMicroAppSSREntry', path.resolve(config.output.get('publicPath'), 'server/index.js'))
+      api.dispatchSync('configMicroAppSSREntry', `${config.output.get('publicPath')}server/index.js`)
     }
   });
 
