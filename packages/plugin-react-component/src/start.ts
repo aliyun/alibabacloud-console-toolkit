5f7dcc0b03db9ba4c96d1a6debdf6e57c1d074f3
yr247308 <yr247308@alibaba-inc.com>
2024-01-02 15:37:56 +0800

    fix: fix syntax error

--- packages/plugin-react-component/src/start.ts
+++ packages/plugin-react-component/src/start.ts
@@ -8,24 +8,23 @@ import type { IBuildOptions } from './type';
 export default async function build(options: IBuildOptions) {
   const { host = '127.0.0.1', port = 3337 } = options;
 
-  try {
-    const server = await createServer({
-      mode: 'development',
-      configFile: false,
-      root: resolve(options.cwd, options.demo || 'demo'),
-      plugins: [
-        tsconfigPaths({
-          root: options.cwd,
-        }),
-        mdPlugin({
-          mode: [Mode.HTML, Mode.TOC, Mode.REACT, Mode.MARKDOWN],
-        }),
-      ],
-      server: {
-        host,
-        port,
-      },
-    });
+  const server = await createServer({
+    mode: 'development',
+    configFile: false,
+    root: resolve(options.cwd, options.demo || 'demo'),
+    plugins: [
+      tsconfigPaths({
+        root: options.cwd,
+      }),
+      mdPlugin({
+        mode: [Mode.HTML, Mode.TOC, Mode.REACT, Mode.MARKDOWN],
+      }),
+    ],
+    server: {
+      host,
+      port,
+    },
+  });
 
   await server.listen();
 
