7f019019c5c8254d566992b6cdb2734c0fee351e
yr247308 <yr247308@alibaba-inc.com>
2024-06-11 16:47:10 +0800

    chore: publish plugin-oneconsole

--- packages/plugin-oneconsole/src/index.ts
+++ packages/plugin-oneconsole/src/index.ts
@@ -1,4 +1,5 @@
 import { PluginAPI, PluginOptions } from '@alicloud/console-toolkit-core';
+import { getEnv } from '@alicloud/console-toolkit-shared-utils';
 
 export default (api: PluginAPI, options: PluginOptions) => {
   const mountDom =
@@ -13,7 +14,27 @@ export default (api: PluginAPI, options: PluginOptions) => {
 <meta data-type="oneconsole.product_global">
 `;
 
-  api.dispatchSync('addHtmlHeadScript', '<meta data-type="oneconsole.basic_header" />');
-  api.dispatchSync('addHtmlHeadScript', '<meta data-type="oneconsole.console_theme"/>');
+  let consoleBaseScript = `<meta data-type="oneconsole.console_bar" data-product="${options.product}">`;
+  let mobileTopBarScript = '<meta data-type="oneconsole.mobile_bar" />';
+  const headerScript = '<meta data-type="oneconsole.basic_header" />';
+  const mobileHeaderScript = '<meta data-type="oneconsole.mobile_basic_header" />';
+
+  if (getEnv().isDev()) {
+    consoleBaseScript = '<script src="//g.alicdn.com/aliyun/console-base-loader/index.js"></script>';
+    mobileTopBarScript = '<link rel="stylesheet" href="https://dev.g.alicdn.com/xconsole-mobile/console-mobile-base/0.1.13/index.css"><script src="https://dev.g.alicdn.com/xconsole-mobile/console-mobile-base/0.1.13/index.js"></script>';
+  }
+
+  const enableConsoleBase = options.consoleBase === true || (typeof options.consoleBase === 'object' && !options.consoleBase.disabled);
+  
   api.dispatchSync('addHtmlPrescript', mountDom);
+  if (options.scene === 'pc') {
+    api.dispatchSync('addHtmlHeadScript', headerScript);
+    // deprecated
+    api.dispatchSync('addHtmlHeadScript', '<meta data-type="oneconsole.console_theme"/>');
+    if (enableConsoleBase) api.dispatchSync('addHtmlScript', consoleBaseScript);
+  }
+  if (options.scene === 'mobile') {
+    api.dispatchSync('addHtmlHeadScript', mobileHeaderScript);
+    if (enableConsoleBase) api.dispatchSync('addHtmlScript', mobileTopBarScript);
+  }
 };
